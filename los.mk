eeb4917c199ef62a8cbf330a4bf8cf1f6899a51f
dopaemon <43093213+dopaemon@users.noreply.github.com>
2019-10-28 11:09:39 +0000

    Update los.mk

--- los.mk
+++ los.mk
@@ -3,7 +3,7 @@ LineageOS Build ( 16.00 )
 Packages :
 udo apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev repo
 
-repo init -u git://github.com/LineageOS/android.git -b lineage-17.0
+repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
 repo sync -j8
 
 nano .repo/local_manifests/roomservice.xml
